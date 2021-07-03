c62f0639abb6133a5163d428e7693cd467b71381
AuStien <audun97@gmail.com>
2021-07-03 16:24:48 +0200

    Init Next.js with TypeScript

--- src/components/auth/Refresh.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Refresh() {
-    return <p>Refresh</p>
-}
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import App from './App'
-import Home from './Home'
-import Callback from './auth/Callback'
-import Header from './Header'
-import Login from './auth/Login'
-import Refresh from './auth/Refresh'
-
-export {App, Home, Callback, Header, Login, Refresh}
