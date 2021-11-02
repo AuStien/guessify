1939ced5ed76dd5bd10fa0d002f4a585c721dae3
Audun Stien <audun97@gmail.com>
2021-11-02 21:21:48 +0100

    Add functionality to Guess.js

--- src/components/guess/Guess.js
+++ src/components/guess/Guess.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { useParams } from "react-router";
 import styled from "styled-components";
 
+import { useFetch } from '../hooks/useFetch'
+import { Context } from '../../Store'
+
 export default function Guess() {
+    const { id } = useParams()
+    const f = useFetch()
+    const [state, dispatch] = useContext(Context)
+
+    useEffect(() => {
+        if(!state.playlists.selected || id != state.playlists.selected.id) {
+            if(state.playlists.list) {
+                for(let p of state.playlists.list) {
+                    if (p.id == id) {
+                        dispatch({type: "SET_SELECTED_PLAYLIST", payload: p})
+                        break
+                    }
+                }
+            }
+        }
+    }, [state.playlists.list])
+
+    useEffect(() => {
+        if(state.playlists.selected) {
+            f("play", state.playlists.selected)
+        }
+    }, [state.playlists.selected])
+
+    if(!state.playlists.selected.name) return <h1>Loading</h1>
+
     return (
         <Frame>
-            asdasd
+            <h1>Playing from '{state.playlists.selected.name}'</h1>
         </Frame>
     )
 }
